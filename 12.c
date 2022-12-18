d5186085328eadf94f93c89621b387d9383fe44c
Chanmin-Hwang <120910557+Chanmin-Hwang@users.noreply.github.com>
2022-12-18 18:23:22 +0900

    Add files via upload

---
+++ 12.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Hello!\nMy name is James Kim.\n Nice to meet you.");
+	return 0;
+}
---
+++ 17.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	int num1, num2;
+	printf("두 정수 값을 입력하세요 :");
+	scanf("%d %d", &num1, &num2);
+	printf("%d * %d = %d",num1,num2,num1*num2);
+	return 0;
+}
