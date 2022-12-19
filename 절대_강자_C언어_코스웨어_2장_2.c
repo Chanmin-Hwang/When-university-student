bf17daa09954dbcc9df7012ec5c0369045133ff5
Chanmin-Hwang <120910557+Chanmin-Hwang@users.noreply.github.com>
2022-12-19 18:22:13 +0900

    Add files via upload

---
+++ 절대_강자_C언어_코스웨어_2장_2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("hello world");
+	/* 주석문 안에 /* 중첩 주석문 */ 실험 */
+	return 0;
+}
---
+++ 절대_강자_C언어_코스웨어_2장_5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("%4d %#4o %#4x\n",1,1,1);
+	printf("%4d %#4o %#4x\n",2,2,2);
+	printf("%4d %#4o %#4x\n",3,3,3);
+	printf("%4d %#4o %#4x\n",4,4,4);
+	printf("%4d %#4o %#4x\n",5,5,5);
+	printf("%4d %#4o %#4x\n",6,6,6);
+	printf("%4d %#4o %#4x\n",7,7,7);
+	printf("%4d %#4o %#4x\n",8,8,8);
+	printf("%4d %#4o %#4x\n",9,9,9);
+	printf("%4d %#4o %#4x\n",10,10,10);
+	printf("%4d %#4o %#4x\n",11,11,11);
+	printf("%4d %#4o %#4x\n",12,12,12);
+	printf("%4d %#4o %#4x\n",13,13,13);
+	printf("%4d %#4o %#4x\n",14,14,14);
+	printf("%4d %#4o %#4x\n",15,15,15);
+	printf("%4d %#4o %#4x\n",16,16,16);
+	printf("%4d %#4o %#4x\n",17,17,17);
+	printf("%4d %#4o %#4x\n",18,18,18);
+	printf("%4d %#4o %#4x\n",19,19,19);
+	printf("%4d %#4o %#4x\n",20,20,20);
+	return 0;
+}
