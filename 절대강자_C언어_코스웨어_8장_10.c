6f207b32d3237a77219ea7caaa5ce004906317e5
Chanmin-Hwang <120910557+Chanmin-Hwang@users.noreply.github.com>
2023-01-10 21:29:52 +0900

    Add files via upload

--- 절대강자_C언어_코스웨어_8장_10.c
+++ 절대강자_C언어_코스웨어_8장_10.c
@@ -1,70 +1,97 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <time.h>
-typedef struct student
-{
-	char name[10];
+typedef struct {
+	char name[50];
 	int num;
 	char grade[3];
 } student;
 int main(void)
 {
-	struct student student[5] = {{"vini",rand()%1000000,"B-"},{"karim",rand()%1000000,"B-"},{"luka",rand()%1000000,"B+"},{"toni",rand()%1000000,"A+"},{"antonio",rand()%1000000,"A0"}};
-	char t[100];
-	int r;
-	for (int k=0;k<4;k++)
-		for (int i=0;i<4;i++)
+	student list[5] = {{"vini",20220020,"A0"},{"toni",20220008,"A+"},{"luka",20220010,"A+"},{"karim",20220009,"B-"},{"antonio",20220022,"A-"}};
+	char t[50];
+	int n;
+	for (int i = 0;i<5;i++)
+		for (int k = 0;k<4;k++)
 		{
-			if (student[i].grade[0] > student[i+1].grade[0])
+			if (list[k].grade[0] > list[k+1].grade[0])
 			{
-				strcpy(t,student[i].grade);
-				strcpy(student[i].grade,student[i+1].grade);
-				strcpy(student[i+1].grade,t);
+				strcpy(t,list[k].grade);
+				strcpy(list[k].grade,list[k+1].grade);
+				strcpy(list[k+1].grade,t);
 
-				strcpy(t,student[i].name);
-				strcpy(student[i].name,student[i+1].name);
-				strcpy(student[i+1].name,t);
+				strcpy(t,list[k].name);
+				strcpy(list[k].name,list[k+1].name);
+				strcpy(list[k+1].name,t);
 
-				r = student[i].num;
-				student[i].num = student[i+1].num;
-				student[i+1].num = r;
+				n = list[k].num;
+				list[k].num = list[k+1].num;
+			    list[k+1].num = n;
 			}
-
-			else if (student[i].grade[0] == student[i+1].grade[0])
+			else if (list[k].grade[0] == list[k+1].grade[0])	
 			{
-				if (student[i].grade[1] > student[i+1].grade[1])
+				if ((list[k].grade[1] == '0') || (list[k+1].grade[1] == '0'))
 				{
-					strcpy(t,student[i].grade);
-					strcpy(student[i].grade,student[i+1].grade);
-					strcpy(student[i+1].grade,t);
-
-					strcpy(t,student[i].name);
-					strcpy(student[i].name,student[i+1].name);
-					strcpy(student[i+1].name,t);
-
-					r = student[i].num;
-					student[i].num = student[i+1].num;
-					student[i+1].num = r;
-				}
-				else if (student[i].grade[1] == student[i+1].grade[1])
-					if (strcmp(student[i].name,student[i+1].name) > 0)
+					if ((list[k].grade[1] == '-') && (list[k+1].grade[1] == '0'))
 					{
-						strcpy(t,student[i].grade);
-						strcpy(student[i].grade,student[i+1].grade);
-						strcpy(student[i+1].grade,t);
+						strcpy(t,list[k].grade);
+						strcpy(list[k].grade,list[k+1].grade);
+						strcpy(list[k+1].grade,t);
 
-						strcpy(t,student[i].name);
-						strcpy(student[i].name,student[i+1].name);
-						strcpy(student[i+1].name,t);
+						strcpy(t,list[k].name);
+						strcpy(list[k].name,list[k+1].name);
+						strcpy(list[k+1].name,t);
 
-						r = student[i].num;
-						student[i].num = student[i+1].num;
-						student[i+1].num = r;
+						n = list[k].num;
+						list[k].num = list[k+1].num;
+						list[k+1].num = n;
 					}
-			}
+					else if ((list[k].grade[1] == '0') && (list[k+1].grade[1] == '+'))
+					{
+						strcpy(t,list[k].grade);
+						strcpy(list[k].grade,list[k+1].grade);
+						strcpy(list[k+1].grade,t);
+
+						strcpy(t,list[k].name);
+						strcpy(list[k].name,list[k+1].name);
+						strcpy(list[k+1].name,t);
+
+						n = list[k].num;
+						list[k].num = list[k+1].num;
+						list[k+1].num = n;
+					}
+				}
+				else if (list[k].grade[1] > list[k+1].grade[1])
+				{
+					strcpy(t,list[k].grade);
+					strcpy(list[k].grade,list[k+1].grade);
+					strcpy(list[k+1].grade,t);
+
+					strcpy(t,list[k].name);
+					strcpy(list[k].name,list[k+1].name);
+					strcpy(list[k+1].name,t);
+
+					n = list[k].num;
+					list[k].num = list[k+1].num;
+			    	list[k+1].num = n;
+				}
+				else if (list[k].grade[1] == list[k+1].grade[1])
+					if (strcmp(list[k].name,list[k+1].name) > 0)
+					{
+						strcpy(t,list[k].grade);
+						strcpy(list[k].grade,list[k+1].grade);
+						strcpy(list[k+1].grade,t);
+	
+						strcpy(t,list[k].name);
+						strcpy(list[k].name,list[k+1].name);
+						strcpy(list[k+1].name,t);
+
+						n = list[k].num;
+						list[k].num = list[k+1].num;
+			    		list[k+1].num = n;
+					}
+			 }
 		}
-	for (int i=0;i<5;i++)
-		printf("%d %s : %s\n",student[i].num,student[i].name,student[i].grade);
+	for (int k = 0;k<5;k++)
+		printf("%s %d %s\n",list[k].name,list[k].num,list[k].grade);
 	return 0;
 }
